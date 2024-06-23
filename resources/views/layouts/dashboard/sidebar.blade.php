64ba237205df4584e54d209fd6e0f2f7f5520595
frllelrtza <alfarrelelmaretza@gmail.com>
2024-06-23 12:33:50 +0700

    [FIX] LOGOUT BUG

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -1,51 +1,61 @@
-   <!-- Sidebar Start -->
-   <aside class="left-sidebar">
-    <!-- Sidebar scroll-->
-    <div>
-      <div class="brand-logo d-flex align-items-center justify-content-between">
-        <a href="/dashboard" class="text-nowrap logo-img">
-          <img class="ms-5" src="{{ asset('assets/dashboard/images/logos/employee.png') }}" width="120" alt="" />
-        </a>
-        <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-          <i class="ti ti-x fs-8"></i>
-        </div>
+<!-- Sidebar Start -->
+<aside class="left-sidebar">
+  <!-- Sidebar scroll-->
+  <div>
+    <div class="brand-logo d-flex align-items-center justify-content-between">
+      <a href="/dashboard" class="text-nowrap logo-img">
+        <img class="ms-5" src="{{ asset('assets/dashboard/images/logos/employee.png') }}" width="120" alt="" />
+      </a>
+      <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+        <i class="ti ti-x fs-8"></i>
       </div>
-      <!-- Sidebar navigation-->
-      <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-        <ul id="sidebarnav">
-          <li class="nav-small-cap">
-            <i class="ti ti-dots nav-small-cap-icon fs-6"></i>
-            <span class="hide-menu">Home</span>
-          </li>
-          <li class="sidebar-item">
-            <a class="sidebar-link" href="/dashboard" aria-expanded="false">
-              <span>
-                <i class="ti ti-layout-dashboard"></i>
-              </span>
-              <span class="hide-menu">Dashboard</span>
-            </a>
-          </li>
-
-          <li class="sidebar-item">
-            <a class="sidebar-link" href="/pegawai" aria-expanded="false">
-              <span>
-                <i class="ti ti-briefcase"></i>
-              </span>
-              <span class="hide-menu">Pegawai</span>
-            </a>
-          </li>
-          <li class="sidebar-item">
-            <a class="sidebar-link" href="/profile" aria-expanded="false">
-              <span>
-                <i class="ti ti-settings "></i>
-              </span>
-              <span class="hide-menu">Setelan</span>
-            </a>
-          </li>
-
-        </ul>
-      </nav>
-      <!-- End Sidebar navigation -->
     </div>
-    <!-- End Sidebar scroll-->
-  </aside>
+    <!-- Sidebar navigation-->
+    <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+      <ul id="sidebarnav">
+        <li class="nav-small-cap">
+          <i class="ti ti-dots nav-small-cap-icon fs-6"></i>
+          <span class="hide-menu">Home</span>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="/dashboard" aria-expanded="false">
+            <span>
+              <i class="ti ti-layout-dashboard"></i>
+            </span>
+            <span class="hide-menu">Dashboard</span>
+          </a>
+        </li>
+
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="/pegawai" aria-expanded="false">
+            <span>
+              <i class="ti ti-briefcase"></i>
+            </span>
+            <span class="hide-menu">Pegawai</span>
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <a class="sidebar-link" href="/profile" aria-expanded="false">
+            <span>
+              <i class="ti ti-settings "></i>
+            </span>
+            <span class="hide-menu">Setelan</span>
+          </a>
+        </li>
+        <li class="sidebar-item">
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a class="sidebar-link" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" aria-expanded="false">
+              <span>
+                <i class="ti ti-user "></i>
+              </span>
+              <span class="hide-menu">Logout</span>
+            </a>
+          </form>
+        </li>
+      </ul>
+    </nav>
+    <!-- End Sidebar navigation -->
+  </div>
+  <!-- End Sidebar scroll-->
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LandingController; // Add this line
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController; // Add this line
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
@@ -20,12 +21,10 @@ require __DIR__.'/auth.php';
 
 Route::resource('/', LandingController::class);
 
-
-
 Route::middleware('auth')->group(function () {
     Route::resource('/profile', ProfileController::class);
     Route::resource('/dashboard', DashboardController::class);
     Route::resource('/pegawai', PegawaiController::class);
-    
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
 
